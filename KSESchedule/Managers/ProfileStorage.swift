541f13e4e321a6ae3cc2149645501dd1ff26599f
andriiklykavkaa <andriiklykavkaa@gmail.com>
2024-10-13 17:39:02 +0300

    Embeded Schedule in Profile Screen

---
+++ KSESchedule/Managers/ProfileStorage.swift
@@ -0,0 +1,8 @@
+//
+//  ProfileStorage.swift
+//  KSESchedule
+//
+//  Created by Andrii Klykavka on 13.10.2024.
+//
+
+import Foundation
---
+++ KSESchedule/Views/KSWebView.swift
@@ -0,0 +1,8 @@
+//
+//  KSWebView.swift
+//  KSESchedule
+//
+//  Created by Andrii Klykavka on 13.10.2024.
+//
+
+import Foundation
